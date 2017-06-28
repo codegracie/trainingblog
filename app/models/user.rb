40babc0301733f54b240f04388bf659f573b3e6c
codegracie <grace.sojor@yahoo.com>
2017-06-28 10:27:44 +0000

    Add password reset edit and update

--- app/models/user.rb
+++ app/models/user.rb
@@ -82,6 +82,11 @@ class User < ActiveRecord::Base
     reset_sent_at < 2.hours.ago
   end
 
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   def feed
     following_ids = "SELECT followed_id FROM relationships
                      WHERE  follower_id = :user_id"
